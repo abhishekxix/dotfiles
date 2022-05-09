fcc05594d97d45dd9a06a140bd5f45be46a24bdd
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-05-09 11:41:59 +0530

    Refacto change_wallpaper.sh

--- change_wallpaper.sh
+++ change_wallpaper.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
-source $HOME/.bash_profile
+source $HOME/.zshenv
 
 declare -x DISPLAY=":0"
 declare -x XAUTHORITY="/home/abhi/.Xauthority"
 
-wallpaper=$(find /home/abhi/wallpapers -type f | shuf -n 1)
+wallpaper=$(find /home/abhi/wallpapers -type f -not -path "*.git*" | shuf -n 1)
 echo $wallpaper > ~/.xwallpaper
 echo $wallpaper | xargs xwallpaper --stretch
