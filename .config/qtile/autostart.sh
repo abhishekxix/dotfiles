d53facc8307eb6399f9fe88d01247bd0fb8d21e2
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-09-22 03:22:32 +0530

    Fix keyring issues

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -2,6 +2,8 @@
 
 lxsession  --session=qtile &
 
+gnome-keyring-daemon --start --login --components=pkcs11,secrets,ssh
+
 xrandr --output HDMI-0 --mode 2560x1440 --pos 1920x0 --rotate normal --output eDP-1-1 --primary --mode 1920x1080 --pos 0x360 --rotate normal
 
 export WINIT_X11_SCALE_FACTOR=1
