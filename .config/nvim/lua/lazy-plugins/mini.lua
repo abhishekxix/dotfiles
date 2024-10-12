96415c569e9774ed68f5318e91bac269333c68f2
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-10-13 02:39:15 +0530

    Nvim: Add some mini plugins

--- .config/nvim/lua/lazy-plugins/mini.lua
+++ .config/nvim/lua/lazy-plugins/mini.lua
@@ -16,6 +16,10 @@ return {
 		-- - sr)'  - [S]urround [R]eplace [)] [']
 		require('mini.surround').setup()
 
+		require('mini.indentscope').setup()
+
+		require('mini.move').setup()
+
 		-- Simple and easy statusline.
 		--  You could remove this setup call if you don't like it,
 		--  and try some other statusline plugin
@@ -30,6 +34,8 @@ return {
 		statusline.section_location = function()
 			return '%2l:%-2v'
 		end
+
+		require('mini.tabline').setup()
 
 		-- ... and there is more!
 		--  Check out: https://github.com/echasnovski/mini.nvim
