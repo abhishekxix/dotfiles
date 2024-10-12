2d5aebdc59ff24b339a1019acf46b3439f61fc02
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-10-12 16:14:49 +0530

    Use blade files as html

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -574,3 +574,11 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=4 sts=4 sw=4
+
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+    pattern = "*.blade.php",
+    callback = function()
+        vim.bo.filetype = "html"
+    end,
+})
+
