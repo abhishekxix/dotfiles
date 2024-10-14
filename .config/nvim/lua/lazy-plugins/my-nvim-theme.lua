9be9b20b5a27c579e0a55c609f795a0f9257fa1a
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-10-14 21:18:38 +0530

    Transparency

--- .config/nvim/lua/lazy-plugins/my-nvim-theme.lua
+++ .config/nvim/lua/lazy-plugins/my-nvim-theme.lua
@@ -33,11 +33,12 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			require('tokyonight').setup({
-				style = "night",
-			})
+			require('tokyonight').setup {
+				style = 'night',
+				transparent = 'true',
+			}
 
 			vim.cmd 'colorscheme tokyonight'
-		end
+		end,
 	},
 }
