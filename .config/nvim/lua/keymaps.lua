78c32ec772cd6b66078741c1184138eaf668dac9
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-10-17 17:53:08 +0530

    NVIM&TMUX: Quality of life improvements

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -9,3 +9,6 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>ad', '"+d', { desc = 'Delete to system clipboard' })
+vim.keymap.set('n', '<leader>ay', '"+y', { desc = 'Yank to system clipboard' })
+vim.keymap.set('n', '<leader>ap', '"+p', { desc = 'Put from system clipboard' })
