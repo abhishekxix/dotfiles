cd835875c8356e6e56caa4a1ee22202c56d73fcd
Abhishek Singh <abhisheksinghxix@gmail.com>
2024-10-19 20:15:35 +0530

    NVIM: Add icon for custom keybinds

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -44,6 +44,7 @@ return {
 			{ '<leader>w', group = '[W]orkspace' },
 			{ '<leader>t', group = '[T]oggle' },
 			{ '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+			{ '<leader>a', group = "Abhishek's Keymaps", mode = { 'n', 'v' }, icon = { icon = '' } },
 		},
 	},
 }
