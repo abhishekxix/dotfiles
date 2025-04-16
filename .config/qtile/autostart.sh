2792ed7ee4ef4052c17f75904f269d04ad085e0e
Abhishek Singh <abhisheksinghxix@gmail.com>
2025-04-17 01:58:42 +0530

    AUDIO: Set echo cancel source as default on startup

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -22,5 +22,5 @@ xargs xwallpaper --stretch <~/.xwallpaper &
 # mictray &
 
 # Set the audio settings.
-pactl load-module module-echo-cancel
+pactl load-module module-echo-cancel source_master=alsa_input.pci-0000_00_1f.3.analog-stereo sink_master=alsa_output.pci-0000_00_1f.3.analog-stereo aec_method=webrtc aec_args="analog_gain_control=0 digital_gain_control=0" use_master_format=yes
 pactl set-default-source echo-cancel-source
