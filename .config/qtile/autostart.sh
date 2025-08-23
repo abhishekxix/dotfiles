ca043fd0d36f5e28a184932f5c49b3ca02f85ad1
Abhishek Singh <abhisheksinghxix@gmail.com>
2025-08-24 04:43:34 +0530

    BACKUP

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/env bash
 
+export XDG_CURRENT_DESKTOP=qtile
+export XDG_SESSION_DESKTOP=qtile
+export XDG_SESSION_TYPE=x11
+
 lxsession --session=qtile &
 
 gnome-keyring-daemon --start --login --components=pkcs11,secrets,ssh
 
-xrandr --output HDMI-0 --mode 2560x1440 --pos 0x0 --rotate normal --output eDP-1-1 --primary --mode 1920x1080 --pos 2560x360 --rotate normal
+xrandr --output HDMI-0 --mode 2560x1440 --rate 75 --pos 0x0 --rotate normal --output eDP-1-1 --primary --mode 1920x1080 --rate 120 --pos 2560x360 --rotate normal
 
 export WINIT_X11_SCALE_FACTOR=1
 
