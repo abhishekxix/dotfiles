1a7b607d35b5f48677daf5b1eabfdb770af71f76
Abhishek Singh <abhisheksinghxix@gmail.com>
2025-12-25 14:44:18 +0530

    NVIM: Fix mini surround mappings

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -10,11 +10,16 @@ return {
 		require('mini.ai').setup { n_lines = 500 }
 
 		-- Add/delete/replace surroundings (brackets, quotes, etc.)
-		--
-		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-		-- - sd'   - [S]urround [D]elete [']quotes
-		-- - sr)'  - [S]urround [R]eplace [)] [']
-		require('mini.surround').setup()
+		require('mini.surround').setup {
+			mappings = {
+				add = '<leader>asa',
+				delete = '<leader>asd',
+				find = '<leader>asf',
+				find_left = '<leader>asF',
+				highlight = '<leader>ash',
+				replace = '<leader>asr',
+			},
+		}
 
 		require('mini.indentscope').setup()
 
