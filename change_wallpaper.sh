8dadac082723d908865e5606123ca84415c2436b
Abhishek Singh <abhisheksinghxix@gmail.com>
2026-01-08 14:17:31 +0530

    CONFIG: Update change wallpaper script

--- change_wallpaper.sh
+++ change_wallpaper.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -e
-source $HOME/.zshenv
+source "$HOME/.zshenv"
 
 #declare -x DISPLAY=":0"
 #declare -x XAUTHORITY="/home/abhi/.Xauthority"
 
-wallpaper=$(find /home/abhi/Pictures/wallpapers -type f -not -path "*.git*" | shuf -n 1)
-echo $wallpaper > ~/.xwallpaper
-echo $wallpaper | xargs xwallpaper --stretch
+wallpaper=""
+
+# Get wallpaper from script arg or pick a random one from the wallpapers directory
+if [[ -n "$1" ]]; then
+  wallpaper=$1
+else
+  wallpaper=$(find "$HOME/Pictures/wallpapers" -type f -not -path "*.git*" | shuf -n 1)
+fi
+
+# wallpaper=$(find /home/abhi/Pictures/wallpapers -type f -not -path "*.git*" | shuf -n 1)
+echo "$wallpaper" >~/.xwallpaper
+echo "$wallpaper" | xargs xwallpaper --stretch
