1209227a2865fbc7ce69c116243f72b2bec409d9
Abhishek Singh <abhisheksinghxix@gmail.com>
2026-09-04 14:09:34 +0530

    NVIM(04): drop invalid catppuccin float option

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -7,9 +7,6 @@ return {
     require('catppuccin').setup {
       flavour = 'mocha',
       transparent_background = true,
-      float = {
-        transparent = true,
-      },
       auto_integrations = true,
     }
     vim.cmd.colorscheme 'catppuccin'
