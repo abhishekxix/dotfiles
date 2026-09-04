c7554eb7ce5c5480f389df0dc0c24727d2e82722
Abhishek Singh <abhisheksinghxix@gmail.com>
2026-09-04 09:26:07 +0530

    NVIM: Remove transparency from previews

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -26,7 +26,6 @@ vim.opt.signcolumn = 'yes'
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.completeopt = 'menu,menuone,noinsert'
-vim.opt.pumblend = 10
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
