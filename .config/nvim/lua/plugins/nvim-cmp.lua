b0157902d69a6365d01678e466d51d0929355f53
Abhishek Singh <abhisheksinghxix@gmail.com>
2026-09-04 08:10:34 +0530

    NVIM(05): normalize cmp (single completeopt, C-Space), guard autopairs cmp wiring

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -13,14 +13,12 @@ return {
       dependencies = {},
     },
     'saadparwaiz1/cmp_luasnip',
-
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
   },
   config = function()
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
-    luasnip.config.setup {}
 
     cmp.setup {
       snippet = {
@@ -28,27 +26,16 @@ return {
           luasnip.lsp_expand(args.body)
         end,
       },
-      completion = { completeopt = 'menu,menuone,noinsert' },
 
       mapping = cmp.mapping.preset.insert {
-        -- ['<C-n>'] = cmp.mapping.select_next_item(),
-        -- ['<C-p>'] = cmp.mapping.select_prev_item(),
-
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
 
-        -- Accept ([y]es) the completion.
-        --  This will auto-import if your LSP supports it.
-        --  This will expand snippets if the LSP sent a snippet.
-        -- ['<C-y>'] = cmp.mapping.confirm { select = true },
-        --
-        -- If you prefer more traditional completion keymaps,
-        -- you can uncomment the following lines
         ['<CR>'] = cmp.mapping.confirm { select = true },
         ['<Tab>'] = cmp.mapping.select_next_item(),
         ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
-        ['<C-_>'] = cmp.mapping.complete {},
+        ['<C-Space>'] = cmp.mapping.complete {},
 
         ['<C-l>'] = cmp.mapping(function()
           if luasnip.expand_or_locally_jumpable() then
@@ -62,10 +49,7 @@ return {
         end, { 'i', 's' }),
       },
       sources = {
-        {
-          name = 'lazydev',
-          group_index = 0,
-        },
+        { name = 'lazydev', group_index = 0 },
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'path' },
